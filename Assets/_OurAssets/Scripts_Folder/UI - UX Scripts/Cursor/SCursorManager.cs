542314195e2173d636839d17f438b976b14f3855
malicehightopp1 <brandin.stanfield@gmail.com>
2025-11-20 11:13:48 -0600

    changes to ui and audio

--- Assets/_OurAssets/Scripts_Folder/UI - UX Scripts/Cursor/SCursorManager.cs
+++ Assets/_OurAssets/Scripts_Folder/UI - UX Scripts/Cursor/SCursorManager.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SCursorManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class SCursorManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler //managing cursor changes on UI hover
 {
     [SerializeField] private Texture2D mDefaultCursor;
     [SerializeField] private Texture2D mPointerCursor;
-    private Vector2 mHotSpot = Vector2.zero;
-    private void Start()
+    private Vector2 mHotSpot = Vector2.zero; //setting hotspot to top left of cursor
+    private void Start() //initial cursor setup
     {
         Cursor.SetCursor(mDefaultCursor, mHotSpot, CursorMode.Auto);
     }
-    public void OnPointerEnter(PointerEventData eventData)
+    public void OnPointerEnter(PointerEventData eventData) //changing cursor when hovering over UI elements
     {
         Cursor.SetCursor(mPointerCursor, mHotSpot, CursorMode.Auto);
     }
-    public void OnPointerExit(PointerEventData eventData)
+    public void OnPointerExit(PointerEventData eventData) //changing cursor back to default when not hovering over UI elements
     {
         Cursor.SetCursor(mDefaultCursor, mHotSpot, CursorMode.Auto);
     }
--- Assets/_OurAssets/Scripts_Folder/UI - UX Scripts/Tweening/SUiMainMenuManager.cs
+++ Assets/_OurAssets/Scripts_Folder/UI - UX Scripts/Tweening/SUiMainMenuManager.cs
@@ -9,23 +9,23 @@ public class SUiMainMenuManager : MonoBehaviour
 
     [SerializeField] private GameObject mSettingsPanel;
     [SerializeField] private GameObject mControlsPanel;
-    private void Awake()
+    private void Awake() //adding listeners to buttons
     {
         mStartButton.onClick.AddListener(() => StartButton());
         mQuitButton.onClick.AddListener(() => QuitButton());
     }
-    private void Start()
+    private void Start() //ensure panels are closed on start
     {
         mSettingsPanel.SetActive(false);
         mControlsPanel.SetActive(false);
     }
-    private void StartButton()
+    private void StartButton() //loading the main game scene
     {
         Debug.Log("Starting the game");
         Time.timeScale = 1.0f;
         SceneManager.LoadScene(1);
     }
-    private void QuitButton()
+    private void QuitButton() //quitting the application
     {
         Debug.Log("Quit button is working");
         Application.Quit();
